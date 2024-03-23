499159bbc86b9056d7e9c07f5085d1101d80ac90
Margosha2002 <margosha161616@gmail.com>
2024-03-23 19:25:54 +0200

    hw5

--- task1.py
+++ task1.py
@@ -1,116 +1,102 @@
-import os
 import sys
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
 
-
-class AlreadyExists(Exception): ...
+from aiopath import Path
+from aioshutil import copyfile
+from loguru import logger
 
 
 class DirectoryDoesNotExist(Exception): ...
 
 
 class ArgumentParser:
-    path: str
-    destination: str
+    path: Path
+    destination: Path
 
-    def __init__(self):
+    @classmethod
+    async def init(cls) -> "ArgumentParser":
+        parser = ArgumentParser()
         argument_amount = len(sys.argv) - 1
         if argument_amount == 0:
-            self.path = os.getcwd()
-            self.destination = os.path.join(self.path, "dist")
+            parser.path = await Path.cwd()
+            parser.destination = parser.path / "dist"
         elif argument_amount == 1:
-            self.path = sys.argv[1]
-            self.check_path(path=self.path)
-            self.destination = os.path.join(self.path, "dist")
+            parser.path = Path(sys.argv[1])
+            await parser.check_path(path=parser.path)
+            parser.destination = parser.path / "dist"
         elif argument_amount == 2:
-            self.path = sys.argv[1]
-            self.check_path(path=self.path)
-            self.destination = sys.argv[2]
+            parser.path = Path(sys.argv[1])
+            await parser.check_path(path=parser.path)
+            parser.destination = Path(sys.argv[2])
+            await parser.check_path(parser.destination)
+        return parser
 
-    def check_path(self, path: str):
-        if not os.path.exists(path):
-            raise DirectoryDoesNotExist(f"Directory {path} does not exist")
+    @staticmethod
+    async def check_path(path: Path):
+        if not await path.is_dir():
+            logger.error(DirectoryDoesNotExist(f"Directory {path} does not exist"))
+            sys.exit(1)
 
 
 class Copyfier:
-    def __init__(self, path: str, destination: str) -> None:
-        self._path: str = self.__get_clean_path(path=path)
-        self._destination: str = self.__get_clean_path(path=destination)
-        self.__absolute_paths_of_copying_files: list[str] = []
-        self.__inner_paths: dict[str, str] = {}
+    _path: Path
+    _destination: Path
+    __absolute_paths_of_copying_files: list[Path]
+    __inner_paths: dict[str, Path]
 
-    def __get_inner_path_for_extention(self, extension: str) -> str:
+    @classmethod
+    async def init(cls, path: Path, destination: Path) -> "Copyfier":
+        cofyfier = Copyfier()
+        cofyfier._path = await path.resolve()
+        cofyfier._destination = await destination.resolve()
+        cofyfier.__absolute_paths_of_copying_files = []
+        cofyfier.__inner_paths = {}
+        return cofyfier
+
+    async def __get_inner_path_for_extension(self, extension: str) -> Path:
         if extension not in self.__inner_paths.keys():
-            self.__inner_paths[extension] = self.__get_clean_path(
-                f"{self._destination}/{extension}"
-            )
-            os.makedirs(name=self.__inner_paths[extension])
+            self.__inner_paths[extension] = self._destination / extension
+            await self.__inner_paths[extension].mkdir(parents=True, exist_ok=True)
         return self.__inner_paths[extension]
 
-    def __get_clean_path(self, path: str) -> str:
-        if path[-1] == "/":
-            path = path[:-1]
-        return path
-
-    def __get_file_extension(self, filename: str) -> str:
+    @staticmethod
+    def __get_file_extension(filename: str) -> str:
         if "." not in filename:
             return "txt"
-        filename = self.__get_clean_path(path=filename)
         return filename[filename.rfind(".") + 1 :]
 
-    def __read_folder(self, path: str) -> None:
-        path = self.__get_clean_path(path)
-        for file in os.listdir(path=path):
-            file_path = f"{path}/{file}"
-            if os.path.isfile(path=file_path):
-                self.__absolute_paths_of_copying_files.append(file_path)
+    async def __read_folder(self, path: Path = None) -> None:
+        if not path:
+            path = self._path
+        async for file in path.iterdir():
+            if file._path == self._destination:
+                continue
+            if await file.is_file():
+                self.__absolute_paths_of_copying_files.append(file)
             else:
-                self.__read_folder(path=file_path)
-
-    async def __aget_files_of(self, path: str) -> None:
-        loop = asyncio.get_running_loop()
-        await loop.run_in_executor(ThreadPoolExecutor(), self.__read_folder, path)
-
-    def __get_filename(self, path: str) -> str:
-        path = self.__get_clean_path(path=path)
-        if "/" not in path:
-            return path
-        return path[path.rfind("/") + 1 :]
-
-    async def __copy_file(self, path: str, destination: str, filename: str):
-        loop = asyncio.get_running_loop()
-        await loop.run_in_executor(
-            ThreadPoolExecutor(), os.popen, f"cp {path} {destination}/{filename}"
-        )
+                await self.__read_folder(file)
 
     async def execute(self) -> None:
-        try:
-            if os.path.exists(path=self._destination):
-                raise AlreadyExists(f"Directory {self._destination} already exists")
-            os.makedirs(name=self._destination)
-            await self.__aget_files_of(path=self._path)
-            for path in self.__absolute_paths_of_copying_files:
-                filename = self.__get_filename(path=path)
-                destination = self.__get_inner_path_for_extention(
-                    extension=self.__get_file_extension(filename=filename)
-                )
-                await self.__copy_file(
-                    path=path, destination=destination, filename=filename
-                )
-        except Exception as e:
-            print(e)
+        await self._destination.mkdir(parents=True, exist_ok=True)
+        await self.__read_folder()
+        for path in self.__absolute_paths_of_copying_files:
+            filename = path.name
+            destination = await self.__get_inner_path_for_extension(
+                extension=self.__get_file_extension(filename=filename)
+            )
+            await copyfile(path, destination / filename)
+
+
+async def main():
+    logger.remove()
+    log_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <blue>{level}</blue> | {message}"
+    logger.add(sys.stdout, format=log_format, colorize=True, level="ERROR")
+    logger.add("file.log", format=log_format, colorize=True, level="ERROR")
+
+    parser = await ArgumentParser.init()
+    copyfier = await Copyfier.init(path=parser.path, destination=parser.destination)
+    await copyfier.execute()
 
 
 if __name__ == "__main__":
-    try:
-        argument_parser = ArgumentParser()
-    except Exception as e:
-        print(e)
-        sys.exit()
-
-    copyfier = Copyfier(
-        path=argument_parser.path, destination=argument_parser.destination
-    )
-
-    asyncio.run(copyfier.execute())
+    asyncio.run(main())
